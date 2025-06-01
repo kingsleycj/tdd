b8b718904843d7039b79bc20c929b741923b0b6d
kingsleycj <primekings.kc@gmail.com>
2025-06-01 02:50:37 +0100

    update test

--- database.js
+++ database.js
@@ -3,14 +3,12 @@ const constants = require('./constant');
 
 const connectDB = async (uri = constants.DATABASE_URI) => {
     try {
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(uri);
         console.log('Database connected');
+        return true;
     } catch (err) {
         console.error('Database connection error:', err);
-        process.exit(1);
+        return false;
     }
 };
 
